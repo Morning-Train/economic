72968611697b3d51d078b756b1ee3f4eaf0af172
Martin Schadegg Brønniche <ms@morningtrain.dk>
2024-02-20 13:57:13 +0100

    ♻️ Changed Lines in invoices to be a Collection instead of array

--- src/Traits/Resources/HasLines.php
+++ src/Traits/Resources/HasLines.php
@@ -2,18 +2,38 @@
 
 namespace Morningtrain\Economic\Traits\Resources;
 
-use Morningtrain\Economic\Resources\Invoice\ProductLine;
+use Illuminate\Support\Collection;
+use Morningtrain\Economic\Attributes\Resources\Properties\ResourceType;
+use Morningtrain\Economic\DTOs\Invoice\ProductLine;
 
 trait HasLines
 {
     /**
-     * @var array ProductLine[]
+     * @var Collection<ProductLine>
      */
-    public array $lines = [];
+    #[ResourceType(ProductLine::class)]
+    public ?Collection $lines = null;
 
     public function addLine(ProductLine $line): static
     {
-        $this->lines[] = $line;
+        if(is_null($this->lines)) {
+            $this->lines = new Collection();
+        }
+
+        $this->lines->add($line);
+
+        return $this;
+    }
+
+    public function addLines(array|Collection $lines): static
+    {
+        foreach ($lines as $line) {
+            if(!is_a($line, ProductLine::class)) {
+                $line = new ProductLine($line);
+            }
+
+            $this->addLine($line);
+        }
 
         return $this;
     }
