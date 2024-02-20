6be3b350127d38f61ec3b5d0771c792df238a52c
Martin Schadegg Brønniche <ms@morningtrain.dk>
2024-02-20 14:00:27 +0100

    ✅ Updated and added invoice tests

--- tests/Unit/InvoiceTest.php
+++ tests/Unit/InvoiceTest.php
@@ -1,161 +1,206 @@
 <?php
 
+use Illuminate\Support\Collection;
 use Morningtrain\Economic\Classes\EconomicResponse;
-use Morningtrain\Economic\DTOs\Note;
-use Morningtrain\Economic\DTOs\Recipient;
+use Morningtrain\Economic\Classes\EconomicCollection;
+use Morningtrain\Economic\DTOs\Invoice\Note;
+use Morningtrain\Economic\DTOs\Invoice\ProductLine;
+use Morningtrain\Economic\DTOs\Invoice\Recipient;
+use Morningtrain\Economic\DTOs\Invoice\Reference;
 use Morningtrain\Economic\Enums\PaymentTermsType;
+use Morningtrain\Economic\Resources\Customer;
 use Morningtrain\Economic\Resources\Invoice\BookedInvoice;
 use Morningtrain\Economic\Resources\Invoice\DraftInvoice;
-use Morningtrain\Economic\Resources\Invoice\ProductLine;
+use Morningtrain\Economic\Resources\Layout;
 use Morningtrain\Economic\Resources\PaymentTerm;
 use Morningtrain\Economic\Resources\Product;
 use Morningtrain\Economic\Resources\VatZone;
 
-it('creates draft invoice', function () {
-    $this->driver->expects()->post()
-        ->withArgs(function (string $url, array $body) {
-            return $url === 'https://restapi.e-conomic.com/invoices/drafts'
-                && $body === [
-                    'customer' => [
-                        'customerNumber' => 1,
-                    ],
-                    'layout' => [
-                        'layoutNumber' => 1,
-                    ],
-                    'currency' => 'DKK',
-                    'paymentTerms' => [
-                        'paymentTermsNumber' => 1,
-                        'paymentTermsType' => 'paidInCash',
-                    ],
-                    'date' => '2021-01-01',
-                    'recipient' => [
-                        'name' => 'John Doe',
-                        'vatZone' => [
-                            'vatZoneNumber' => 1,
-                        ],
-                    ],
-                    'lines' => [
-                        [
-                            'description' => 'T-shirt - Size L',
-                            'product' => [
-                                'productNumber' => '1',
-                            ],
-                            'quantity' => 1,
-                            'unitNetPrice' => 500,
-                        ],
-                    ],
-                    'notes' => [
-                        'heading' => 'Heading',
-                        'textLine1' => 'Text line 1',
-                        'textLine2' => 'Text line 2',
-                    ],
-                ];
-        })
-        ->once()
-        ->andReturn(new EconomicResponse(201, [
-            'draftInvoiceNumber' => 1,
-            'recipient' => [
-                'name' => 'John Doe',
-                'vatZone' => [
-                    'vatZoneNumber' => 1,
-                ],
-            ],
-        ]));
+it('gets a drafted invoice', function() {
+   $this->driver->expects()->get(
+       'https://restapi.e-conomic.com/invoices/drafts/422',
+       []
+   )->andReturn(new EconomicResponse(200, fixture('Invoices/draft/get-single')));
 
-    DraftInvoice::new(
-        1,
-        1,
+    $invoice = DraftInvoice::find(422);
+
+    expect($invoice)
+        ->toBeInstanceOf(DraftInvoice::class)
+        ->draftInvoiceNumber->toBe(422)
+        ->externalId->toBe('123456')
+        ->date->toBeInstanceOf(DateTime::class)
+        ->currency->toBe('DKK')
+        ->exchangeRate->toBeFloat()
+        ->netAmount->toBeFloat()
+        ->grossAmount->toBeFloat()
+        ->grossAmountInBaseCurrency->toBeFloat()
+        ->marginInBaseCurrency->toBeFloat()
+        ->marginPercentage->toBeFloat()
+        ->vatAmount->toBeFloat()
+        ->roundingAmount->toBeFloat()
+        ->costPriceInBaseCurrency->toBeFloat()
+        ->dueDate->toBeInstanceOf(DateTime::class)
+        ->paymentTerms->toBeInstanceOf(PaymentTerm::class)
+        ->customer->toBeInstanceOf(Customer::class)
+        ->recipient->toBeInstanceOf(Recipient::class)
+        ->notes->toBeInstanceOf(Note::class)
+        ->references->toBeInstanceOf(Reference::class)
+        ->layout->toBeInstanceOf(Layout::class)
+        ->paymentTerms->toBeInstanceOf(PaymentTerm::class)
+        ->lines->toBeInstanceOf(Collection::class)
+        ->lines->first()->toBeInstanceOf(ProductLine::class);
+});
+
+it('gets all drafted invoices', function() {
+    $this->driver->expects()->get(
+        'https://restapi.e-conomic.com/invoices/drafts',
+        [
+            'pageSize' => 20,
+            'skipPages' => 0,
+        ]
+    )->andReturn(new EconomicResponse(200, fixture('Invoices/draft/get-collection')));
+
+    $invoices = DraftInvoice::all();
+
+    expect($invoices)->toBeInstanceOf(EconomicCollection::class);
+    expect($invoices->first())->toBeInstanceOf(DraftInvoice::class);
+    expect($invoices->all())->toHaveCount(2);
+});
+
+it('creates a draft invoice using create', function() {
+    $this->driver->expects()->post()->with(
+        'https://restapi.e-conomic.com/invoices/drafts',
+        fixture('Invoices/draft/create-request')
+    )->andReturn(new EconomicResponse(201, fixture('Invoices/draft/create-response')));
+
+
+    $invoice = DraftInvoice::create(
         'DKK',
-        PaymentTerm::new(
-            paymentTermsNumber: 1,
-            paymentTermsType: PaymentTermsType::PAID_IN_CASH,
-        ),
-        DateTime::createFromFormat('Y-m-d', '2021-01-01'),
+        1,
+        new DateTime('2024-02-13T12:20:18+00:00'),
+        14,
+        1,
         Recipient::new(
             'John Doe',
             new VatZone(1),
         ),
-        Note::new(
+        [
+            ProductLine::new(
+                description: 'T-shirt - Size L',
+                product: new Product([
+                    'productNumber' => 1,
+                ]),
+                quantity: 1,
+                unitNetPrice: 500
+            )
+        ],
+        notes: Note::new(
             heading: 'Heading',
             textLine1: 'Text line 1',
             textLine2: 'Text line 2'
         )
-    )
-        ->addLine(ProductLine::new(
+    );
+
+    expect($invoice)
+        ->toBeInstanceOf(DraftInvoice::class)
+        ->draftInvoiceNumber->toBe(424);
+});
+
+it('creates a draft invoice using new and save', function() {
+    $this->driver->expects()->post()->with(
+        'https://restapi.e-conomic.com/invoices/drafts',
+        fixture('Invoices/draft/create-request')
+    )->andReturn(new EconomicResponse(201, fixture('Invoices/draft/create-response')));
+
+    $invoice = DraftInvoice::new(
+        'DKK',
+        1,
+        new DateTime('2024-02-13T12:20:18+00:00'),
+        14,
+        1,
+        Recipient::new(
+            'John Doe',
+            new VatZone(1),
+        ),
+        notes: Note::new(
+            heading: 'Heading',
+            textLine1: 'Text line 1',
+            textLine2: 'Text line 2'
+        )
+    );
+
+    $invoice->addLine(
+        ProductLine::new(
             description: 'T-shirt - Size L',
             product: new Product([
                 'productNumber' => 1,
             ]),
             quantity: 1,
             unitNetPrice: 500
-        ))
-        ->create();
+        )
+    );
+
+    $invoice->save();
+
+    expect($invoice)
+        ->toBeInstanceOf(DraftInvoice::class)
+        ->draftInvoiceNumber->toBe(424);
 });
 
 it('books draft invoice', function () {
-    $this->driver->expects()->post()
-        ->withArgs(function (string $url, array $body) {
-            return $url === 'https://restapi.e-conomic.com/invoices/drafts';
-        })
-        ->once()
-        ->andReturn(new EconomicResponse(201, [
-            'draftInvoiceNumber' => 1,
-        ]));
-
-    $this->driver->expects()->post()
-        ->withArgs(function (string $url, array $body) {
-            return $url === 'https://restapi.e-conomic.com/invoices/booked'
-                && $body === [
-                    'draftInvoice' => [
-                        'draftInvoiceNumber' => 1,
-                    ],
-                ];
-        })
-        ->once()
-        ->andReturn(new EconomicResponse(201, []));
-
-    DraftInvoice::new(
-        1,
-        1,
-        'DKK',
-        1,
-        DateTime::createFromFormat('Y-m-d', '2021-01-01'),
-        new Recipient(
-            'John Doe',
-            new VatZone(1),
-        )
+    $this->driver->expects()->post(
+        'https://restapi.e-conomic.com/invoices/booked',
+        fixture('Invoices/draft/book-request')
     )
-        ->create()
-        ->book();
+        ->once()
+        ->andReturn(new EconomicResponse(
+            201,
+            fixture('Invoices/draft/book-response')
+        ));
+
+    $invoice = new DraftInvoice(424);
+
+    $bookedInvoice = $invoice->book();
+
+    expect($bookedInvoice)
+        ->toBeInstanceOf(BookedInvoice::class)
+        ->bookedInvoiceNumber->toBe(300);
 });
 
 it('can add lines', function () {
-    $invoice = DraftInvoice::new(
+    $invoice = DraftInvoice::new('DKK',
         1,
+        new DateTime('2024-02-13T12:20:18+00:00'),
+        14,
         1,
-        'DKK',
-        1,
-        DateTime::createFromFormat('Y-m-d', '2021-01-01'),
-        new Recipient(
+        Recipient::new(
             'John Doe',
             new VatZone(1),
         )
-    )
-        ->addLine(ProductLine::new(
+    );
+
+    $invoice->addLine(ProductLine::new(
             product: new Product(1),
             quantity: 1,
             unitNetPrice: 500
-        ))
-        ->addLine(ProductLine::new(
-            product: new Product(2),
-            quantity: 5,
-            unitNetPrice: 100,
+    ))->addLines([
+        [
+            'product' => new Product(2),
+            'quantity' => 2,
+            'unitNetPrice' => 100,
+            'description' => 'Some description'
+        ],
+        ProductLine::new(
+            product: new Product(3),
+            quantity: 3,
+            unitNetPrice: 200,
             description: 'Some description',
-        ));
+        )
+    ]);
 
     expect($invoice->lines)
-        ->toBeArray()
-        ->toHaveCount(2);
+        ->toBeInstanceOf(Collection::class)
+        ->toHaveCount(3);
 
     expect($invoice->lines[0])
         ->toBeInstanceOf(ProductLine::class)
@@ -173,78 +218,21 @@ it('can add lines', function () {
         ->marginInBaseCurrency->toBeNull()
         ->marginPercentage->toBeNull()
         ->product->toBeInstanceOf(Product::class)
-        ->quantity->toBe(5.0);
-});
-
-it('returns expected booked invoice data', function () {
-    $this->driver->expects()->post()
-        ->withArgs(function (string $url, array $body) {
-            return $url === 'https://restapi.e-conomic.com/invoices/booked'
-                && $body === [
-                    'draftInvoice' => [
-                        'draftInvoiceNumber' => 1,
-                    ],
-                ];
-        })
-        ->once()
-        ->andReturn(new EconomicResponse(201, [
-            'bookedInvoiceNumber' => 1,
-        ]));
-
-    $bookedInvoice = BookedInvoice::createFromDraft(1);
-
-    expect($bookedInvoice)
-        ->bookedInvoiceNumber->toBe(1);
-});
-
-it('handles populating resource with enum value', function () {
-    $this->driver->expects()->post()
-        ->withAnyArgs()
-        ->once()
-        ->andReturn(new EconomicResponse(201, [
-            'draftInvoiceNumber' => 1,
-            'recipient' => [
-                'name' => 'John Doe',
-                'vatZone' => [
-                    'vatZoneNumber' => 1,
-                ],
-            ],
-            'paymentTerms' => [
-                'paymentTermsNumber' => 1,
-                'paymentTermsType' => 'net',
-            ],
-        ]), new EconomicResponse(201, [
-            'bookedInvoiceNumber' => 1,
-        ]));
-
-    DraftInvoice::new(
-        1,
-        1,
-        'DKK',
-        PaymentTerm::new(
-            paymentTermsNumber: 1,
-        ),
-        DateTime::createFromFormat('Y-m-d', '2021-01-01'),
-        Recipient::new(
-            'John Doe',
-            new VatZone(1),
-        )
-    )
-        ->create();
+        ->quantity->toBe(2.0);
 });
 
 it('can add notes', function () {
     $invoice = DraftInvoice::new(
-        1,
-        1,
         'DKK',
         1,
-        DateTime::createFromFormat('Y-m-d', '2021-01-01'),
-        new Recipient(
+        new DateTime('2024-02-13T12:20:18+00:00'),
+        14,
+        1,
+        Recipient::new(
             'John Doe',
             new VatZone(1),
         ),
-        Note::new(
+        notes: Note::new(
             heading: 'Heading',
             textLine1: 'Text line 1',
             textLine2: 'Text line 2'
