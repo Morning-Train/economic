df8f2c5aca5fa43f6edb46a74d4099bd64147815
Lars Kjær Rasmussen <laras@morningtrain.dk>
2024-03-14 16:56:23 +0100

    ✨ Add option to get pdf content response to pdf

--- src/DTOs/Invoice/Pdf.php
+++ src/DTOs/Invoice/Pdf.php
@@ -3,8 +3,17 @@
 namespace Morningtrain\Economic\DTOs\Invoice;
 
 use Morningtrain\Economic\Abstracts\Resource;
+use Morningtrain\Economic\Classes\EconomicResponse;
+use Morningtrain\Economic\Services\EconomicApiService;
 
 class Pdf extends Resource
 {
     public ?string $download;
+
+    public function getPdfContentResponse(): EconomicResponse
+    {
+        $url = $this->download;
+
+        return EconomicApiService::get($url);
+    }
 }
